5423ea4c777a6b9da959291b0011280da2871d01
lawrencemah <lawma17@yahoo.com>
2017-03-09 18:21:58 +0800

    Update project.js

--- test/project.js
+++ test/project.js
@@ -1,27 +1,42 @@
 contract('Project', function(accounts) {
-  it("should add 2 contributions of 0.1 ether and refund them", function() {
+  it("contributed by account1", function() {
     var project = Project.deployed();
-    var contributionAmount = 100000000000000000
-
+    var contributionAmount = 100000000;
+    var account1BalanceBefore = web3.eth.getBalance(accounts[3]).toNumber();
+    console.log("BALANCE BEFORE " + account1BalanceBefore);
     //send 0.1 ether from account 1
-    return project.fund(accounts[1],{value : contributionAmount, from: accounts[1]})
-    .then(function() {
-       //send 0.1 ether from account 2
-       return project.fund(accounts[2],{value : contributionAmount, from: accounts[2]})
-      .then(function() {
-        var account1BalanceBefore = web3.eth.getBalance(accounts[1]).toNumber();
-        var account2BalanceBefore = web3.eth.getBalance(accounts[2]).toNumber();
-        //refund contributions
-        return project.refund()
-        .then(function() {
-          //current balance = old balance + contribution amount
-          assert.equal(web3.eth.getBalance(accounts[1]).toNumber(), account1BalanceBefore + contributionAmount, "Incorrect amount refunded for contribution 1")
-          assert.equal(web3.eth.getBalance(accounts[2]).toNumber(), account2BalanceBefore + contributionAmount, "Incorrect amount refunded for contribution 2")
-          //make sure project has 0 balance remaining
-          assert.equal(web3.eth.getBalance(project.address).toNumber(), 0, "Project still has remaining ether");
-          })
-      });
+    return project.fund(accounts[3],{value : contributionAmount, from: accounts[3]})
+    .then(function(tx) {
+      var gasUsed = web3.eth.getTransactionReceipt(tx).gasUsed;
+      console.log(gasUsed);
+      //turn gasUsed_upper into 8389200000000000
+      var gasUsedUpperLimit = 8389300000000000;
+      //gas Used lower = 8389100000000000
+      var gasUsedLowerLimit=8389000000000000;
+      console.log ("BALANCE : " + web3.eth.getBalance(accounts[3]).toNumber());
+      console.log(( account1BalanceBefore - web3.eth.getBalance(accounts[3]).toNumber()));
+      assert.equal(true,(web3.eth.getBalance(accounts[3]).toNumber() > (account1BalanceBefore - (contributionAmount + gasUsedUpperLimit ))),"Incorrect")
+      assert.equal(true,(web3.eth.getBalance(accounts[3]).toNumber() < (account1BalanceBefore - (contributionAmount + gasUsedLowerLimit))) ,"Incorrect")
+    
     });
   });
+  it("refund to account 1",function() {
+     var project = Project.deployed();
+     var contributionAmount = 100000000;
+     var account1Balance = web3.eth.getBalance(accounts[3]).toNumber();
+     return project.refund()
+      .then (function(tx){
+      var gasUsed = web3.eth.getTransactionReceipt(tx).gasUsed;
+      console.log(gasUsed);
+      console.log(( web3.eth.getBalance(accounts[3]).toNumber())- account1Balance);
+      assert.equal(true,(web3.eth.getBalance(accounts[3]).toNumber() = (account1Balance + contributionAmount)),"Incorrect")
+    
+
+    })
+
+   });
+
+
+
 });
 
